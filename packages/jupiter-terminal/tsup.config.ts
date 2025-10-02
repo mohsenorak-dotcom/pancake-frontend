8f36fac75f47fc61dae6a272c78878ca59480cb5
memoyil <2213635+memoyil@users.noreply.github.com>
2025-10-02 09:28:53 +0200

    chore: Bump up target

--- packages/jupiter-terminal/tsup.config.ts
+++ packages/jupiter-terminal/tsup.config.ts
@@ -11,7 +11,7 @@ export default defineConfig((options) => ({
   treeshake: true,
   splitting: true,
   external: ['react', 'react-dom', '@solana/web3.js'],
-  target: 'es6',
+  target: 'es2020',
   cjsInterop: true,
   onSuccess: async () => {
     exec('tsc --emitDeclarationOnly --declaration', (err, stdout) => {
