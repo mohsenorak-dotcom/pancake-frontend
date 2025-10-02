8fc33274a290f0052746a3021009e30b669196ad
memoyil <2213635+memoyil@users.noreply.github.com>
2025-10-02 11:48:06 +0200

    fix: Format

--- packages/jupiter-terminal/overrides/src/contexts/USDValueProvider.tsx
+++ packages/jupiter-terminal/overrides/src/contexts/USDValueProvider.tsx
@@ -74,9 +74,9 @@ export const USDValueProvider: FC<PropsWithChildren<IInit>> = ({ children }) => 
   )
 
   const getPriceFromJupAPI = useCallback(async (addresses: string[]) => {
-    const { data }: { data: JupPriceResponse } = await fetch(
-      `https://lite-api.jup.ag/price/v3?ids=${addresses.join(',')}`,
-    ).then((res) => res.json())
+    const data: JupPriceResponse = await fetch(`https://lite-api.jup.ag/price/v3?ids=${addresses.join(',')}`).then(
+      (res) => res.json(),
+    )
 
     const nowTimestamp = new Date().getTime()
     const result = addresses.reduce<{ result: Record<string, CacheUSDValue>; failed: string[] }>(
