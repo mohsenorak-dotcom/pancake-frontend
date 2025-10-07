f029538a59f2d39791993e973be0f0dedd9c4500
memoyil <2213635+memoyil@users.noreply.github.com>
2025-10-07 19:52:23 +0200

    chore: Change solana link to pcs

--- apps/web/src/wallet/hook/useSwitchNetworkV2.ts
+++ apps/web/src/wallet/hook/useSwitchNetworkV2.ts
@@ -220,7 +220,7 @@ const useProcessSwitchChainRequest = () => {
 
         // Solana
         if (!SOLANA_SUPPORTED_PATH.some((p) => path.startsWith(p))) {
-          window.open('https://solana.pancakeswap.finance', '_self')
+          window.open('https://pancakeswap.finance/swap?chain=sol', '_self')
           return true
         }
         updateAccountState((prev) => ({
