82ab7655ea8a6e51053a5e60e13165aba0798c17
memoyil <2213635+memoyil@users.noreply.github.com>
2025-12-02 06:22:58 +0100

    chore: Bump up layerzero bridge and remove unused

--- apps/bridge/components/layerZero/config.ts
+++ apps/bridge/components/layerZero/config.ts
@@ -1,6 +1,6 @@
-const VERSION = '0.0.32'
+const VERSION = '0.0.34'
 // https://unpkg.com/@layerzerolabs/x-pancakeswap-widget@0.0.21/element.mjs.sha384
-const SHA384 = 'AGUVeA2krFQS5NHl3wT//Pm0uIoO1uSIf6Za0Dtj1kQQQNC8qrq6TjSt7xrwgH0J'
+const SHA384 = 'wy9orblleJ9xshPiuB6uHDL8DKLf/yJO5C4jYNTx43aUAJs9awXFAe8hk3tqGCM+'
 
 export const LAYER_ZERO_JS = {
   src: `https://unpkg.com/@layerzerolabs/x-pancakeswap-widget@${VERSION}/element.mjs`,
